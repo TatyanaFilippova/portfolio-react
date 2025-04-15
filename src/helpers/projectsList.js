f3f2c4579d3485d48b9e250a6b3ad14c70e3c70b
Tatyana <filippovat42@gmail.com>
2025-04-15 20:15:43 +0300

    добавила страницы skills и project

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,25 +1,30 @@
-import magicVineShop from "../img/projects/magicVineShop.jpg"
-import todoList from "../img/projects/todoList.png"
-import weather from "../img/projects/weather.png"
-
+import magicVineShop from "../img/projects/magicVineShop.jpg";
+import todoList from "../img/projects/todoList.png";
+import weather from "../img/projects/weather.png";
 
 export const projectsList = [
+  {
+    title: "Magic-vine-shop",
+    img: magicVineShop,
+    skills: "Skills: React, Node.js",
+    gitHubLink: "https://github.com/TatyanaFilippova/Magic-vine-shop",
+  },
+  {
+    title: "Weather-forecast",
+    img: weather,
+    skills: "Skills: React, Node.js",
+    gitHubLink: "https://github.com/TatyanaFilippova/ weather-forecast-react",
+  },
+  {
+    title: "Todo-list",
+    img: todoList,
+    skills: "Skills: React",
+    gitHubLink: "https://github.com/TatyanaFilippova/todo-list",
+  },
     {
-        title: 'Magic-vine-shop',
-        img: magicVineShop,
-        skills: 'Skills: React, Node.js',
-        gitHubLink: 'https://github.com/TatyanaFilippova/Magic-vine-shop'
-    },
-    {
-        title: 'Weather-forecast-react',
-        img: weather,
-        skills: 'Skills: React, Node.js',
-        gitHubLink: 'https://github.com/TatyanaFilippova/ weather-forecast-react'
-    },
-    {
-        title: 'Todo-list',
+        title: "Todo-list",
         img: todoList,
-        skills: 'Skills: React',
-        gitHubLink: 'https://github.com/TatyanaFilippova/todo-list'
+        skills: "Skills: React",
+        gitHubLink: "https://github.com/TatyanaFilippova/todo-list",
     },
-]
+];
