e80ab8740fa7a9fd221561aaa7b28ea09b0ad4c6
Tatyana <filippovat42@gmail.com>
2025-06-27 13:17:45 +0300

    img

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -1,12 +1,24 @@
-import todoList from "../img/projects/todoList.png";
-import weather from "../img/projects/weather.png";
-import img1 from "../img/projects/magicVineShop/img1.jpg";
-import img2 from "../img/projects/magicVineShop/img2.jpg";
-import img3 from "../img/projects/magicVineShop/img3.jpg";
-import img4 from "../img/projects/magicVineShop/img4.jpg";
-import magicVineShop from "../img/projects/magicVineShop/magicVineShop.jpg";
+import todoList from "../img/projects/toDoList_1/todoList.png";
+import weather from "../img/projects/weather/weather.png";
+import img1_magicVineShop from "../img/projects/magicVineShop/img1.png";
+import img2_magicVineShop from "../img/projects/magicVineShop/img2.png";
+import img3_magicVineShop from "../img/projects/magicVineShop/img3.png";
+import img4_magicVineShop from "../img/projects/magicVineShop/img4.png";
+import magicVineShop from "../img/projects/magicVineShop/magicVineShop.png";
 import facadySysoly from "../img/projects/facadySysoly/facadySysoly.png";
-import toDoList2 from "../img/projects/toDoList2/toDoList2.png"
+import toDoList2 from "../img/projects/toDoList_2/toDoList2.png"
+import img1_toDoList_1 from "../img/projects/toDoList_1/img1.png";
+import img2_toDoList_1 from "../img/projects/toDoList_1/img2.png";
+import img3_toDoList_1 from "../img/projects/toDoList_1/img3.png";
+import img4_toDoList_1 from "../img/projects/toDoList_1/img4.png";
+import img1_weather from "../img/projects/weather/img1.png";
+import img2_weather from "../img/projects/weather/img2.png";
+import img3_weather from "../img/projects/weather/img3.png";
+import img4_weather from "../img/projects/weather/img4.png";
+import img5_weather from "../img/projects/weather/img5.png";
+
+
+
 
 export const projectsList = [
   {
@@ -14,19 +26,22 @@ export const projectsList = [
     img: magicVineShop,
     skills: "Skills: React, Node.js",
     gitHubLink: "https://github.com/TatyanaFilippova/Magic-vine-shop",
-    imgList: [img4, img2, img3, img1],
+    imgList: [img1_magicVineShop, img2_magicVineShop, img3_magicVineShop, img4_magicVineShop],
   },
   {
     title: "Weather forecast",
     img: weather,
     skills: "Skills: React, Node.js",
     gitHubLink: "https://github.com/TatyanaFilippova/weather-forecast-react",
+    imgList: [img1_weather, img2_weather, img3_weather, img4_weather, img5_weather],
   },
   {
     title: "To-do list",
     img: todoList,
     skills: "Skills: React",
     gitHubLink: "https://github.com/TatyanaFilippova/todo-list",
+    imgList: [img1_toDoList_1, img2_toDoList_1, img3_toDoList_1, img4_toDoList_1],
+
   },
   {
     title: "Fasady sysoly" ,
@@ -35,7 +50,7 @@ export const projectsList = [
     gitHubLink: "https://github.com/TatyanaFilippova/fasady-sysoly",
   },
   {
-    title: "To-do list-2" ,
+    title: "To-do list 2" ,
     img: toDoList2,
     skills: "Skills: React",
     gitHubLink: "https://github.com/TatyanaFilippova/homework-2-lad-academy",
